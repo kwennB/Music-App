1b4669e8e36b930889a7c764bd9d7aa8e463c8ae
KwennB <anyebeblessing737@yahoo.com>
2021-05-02 03:24:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,106 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+//Song Title
+const songs = ['Silence', 'Slow-grind', 'Sunset-eyes'];
+
+//Keep track of song
+let songIndex = 1;
+
+//load song details into DOM
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song){
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `img/${song}.jpg`
+}
+
+//Play song
+function playSong(){
+    musicContainer.classList.add("play");
+    playBtn.querySelector("i.fas").classList.remove("fa-play");
+    playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+    audio.play();
+}
+
+//Pause song
+function pauseSong() {
+    musicContainer.classList.remove("play");
+    playBtn.querySelector("i.fas").classList.add("fa-play");
+    playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+    audio.pause();
+}
+
+//Previous song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length-1;
+    }
+    loadSong(songs[songIndex]);
+             playSong(); void
+    playSong();
+}
+
+//Next song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length -1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+//Update progress bar
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width =`${progressPercent}%`;
+}
+
+//set progress bar
+function setProgress (e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width ) * duration;
+}
+
+//event listener
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying){
+        pauseSong();
+    }else {
+        playSong();
+    }
+});
+
+//Change song
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+//Timeupdate song
+audio.addEventListener("timeupdate",updateProgress);
+
+//click on progress bar
+progressContainer.addEventListener("click", setProgress);
+
+//song ends
+audio.addEventListener("ended", nextSong);
